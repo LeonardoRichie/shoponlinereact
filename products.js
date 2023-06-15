38fa01ff4fff5b734248fd6b797e2fa870d23cca
LeonardoRichie <91533882+LeonardoRichie@users.noreply.github.com>
2023-06-15 23:09:06 +0700

    updated

--- products.js
+++ products.js
@@ -1,67 +1,67 @@
 import product1 from './assets/1.png'
-import product2 from './assets/2.png'
+import product2 from './assets/2.jpg'
 import product3 from './assets/3.png'
-import product4 from './assets/4.png'
-import product5 from './assets/5.png'
-import product6 from './assets/6.webp'
-import product7 from './assets/7.webp'
-import product8 from './assets/8.webp'
+import product4 from './assets/4.jpg'
+import product5 from './assets/5.jpg'
+import product6 from './assets/6.png'
+import product7 from './assets/7.png'
+import product8 from './assets/8.jpg'
 
 export const PRODUCTS = [
     {
         id: 1,
         productName: 'IPhone 14',
-        price: 999.0,
+        price: 1200.0,
         productImage: product1,
-        description: 'made in china'
+        description: 'The iPhone 14 display has rounded corners that follow a beautiful curved design, and these corners are within a standard rectangle. When measured as a standard rectangular shape, the screen is 6.06 inches diagonally (actual viewable area is less).'
     },
     {
         id: 2,
         productName: 'IPhone 14 Pro',
         price: 999.0,
         productImage: product2,
-        description: 'made in china'
+        description: 'The iPhone 14 Pro and iPhone 14 Pro Max are smartphones designed, developed, and marketed by Apple Inc. They are the sixteenth-generation flagship iPhones, succeeding the iPhone 13 Pro and iPhone 13 Pro Max. The devices were unveiled alongside the iPhone 14 and iPhone 14 Plus during the Apple Event at Apple Park in Cupertino, California, on September 7, 2022.[7][8] Pre-orders for the iPhone 14 Pro and 14 Pro Max began on September 9, 2022, and were made available on September 16, 2022.'
     },
     {
         id: 3,
         productName: 'IPhone 14 Pro Max',
         price: 999.0,
         productImage: product3,
-        description: 'made in china'
+        description: 'The iPhone 14 Pro and iPhone 14 Pro Max are smartphones designed, developed, and marketed by Apple Inc. They are the sixteenth-generation flagship iPhones, succeeding the iPhone 13 Pro and iPhone 13 Pro Max. The devices were unveiled alongside the iPhone 14 and iPhone 14 Plus during the Apple Event at Apple Park in Cupertino, California, on September 7, 2022.[7][8] Pre-orders for the iPhone 14 Pro and 14 Pro Max began on September 9, 2022, and were made available on September 16, 2022.'
     },
     {
         id: 4,
-        productName: 'IPhone 15',
+        productName: 'Samsung Galaxy S23 ULTRA',
         price: 999.0,
         productImage: product4,
-        description: 'made in china'
+        description: 'The Galaxy S23 Ultra has a squarer form factor than the rest of the series because it’s the only variant to adopt the S Pen. The back panel and display curve around the side edges, but only slightly. The S23 Ultra is flatter than the Galaxy Note series used to be.'
     },
     {
         id: 5,
-        productName: 'IPhone 15 Pro',
+        productName: 'Nintendo Switch',
         price: 999.0,
         productImage: product5,
-        description: 'made in china'
+        description: 'hybrid video game console, consisting of a console unit, a dock, and two Joy-Con controllers. '
     },
     {
         id: 6,
-        productName: 'IPhone 15 Pro Max',
+        productName: 'PS5',
         price: 999.0,
         productImage: product6,
-        description: 'made in china'
+        description: 'Experience lightning-fast loading with an ultra-high speed SSD, deeper immersion with support for haptic feedback1, adaptive triggers1 and 3D Audio*, and an all-new generation of incredible PlayStation games.'
     },
     {
         id: 7,
-        productName: 'IPhone',
+        productName: 'Ipad',
         price: 999.0,
         productImage: product7,
-        description: 'made in china'
+        description: ' runs iOS and ships with several popular Apple and third-party apps preinstalled, including Messages, Mail, FaceTime, Music, Photos and the Safari web browser. Users can also download additional free and paid apps through the Apple App Store.'
     },
     {
         id: 8,
-        productName: 'IPhone',
+        productName: 'Macbook',
         price: 999.0,
         productImage: product8,
-        description: 'made in china'
+        description: ' a term used for a brand of Mac notebook computers that Apple started producing in 2006. The American multinational corporation created MacBook computers when it consolidated its PowerBook and iBook lines during its transition to Intel processor-based products. As of 2013, there are two types of MacBook computers: the base-level MacBook Air and the upper-level MacBook Pro.'
     },
 ]
