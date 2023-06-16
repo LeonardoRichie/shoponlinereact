435df5a3c4ca4f0673ef21455a149c15dc62a25c
raissaazaria <91534032+raissaazaria@users.noreply.github.com>
2023-06-16 23:04:17 +0700

    Add files via upload

--- src/products.js
+++ src/products.js
@@ -6,63 +6,95 @@ import product5 from './assets/5.jpg'
 import product6 from './assets/6.png'
 import product7 from './assets/7.png'
 import product8 from './assets/8.jpg'
+import product9 from './assets/9.jpg'
+import product10 from './assets/10.jpg'
+import product11 from './assets/11.jpg'
+import product12 from './assets/12.jpg'
+
 
 export const PRODUCTS = [
     {
         id: 1,
-        productName: 'IPhone 14',
-        price: 1200.0,
+        productName: 'Sparkly Ring',
+        price: 1.299,
         productImage: product1,
-        description: 'The iPhone 14 display has rounded corners that follow a beautiful curved design, and these corners are within a standard rectangle. When measured as a standard rectangular shape, the screen is 6.06 inches diagonally (actual viewable area is less).'
+        description: 'Elegance meets brilliance in this captivating diamond ring. Crafted with precision, it features a dazzling centerpiece—a brilliant-cut diamond of exceptional quality. Set in a lustrous band, this exquisite ring is a timeless symbol of sophistication. Own a piece of everlasting beauty today.'
     },
     {
         id: 2,
-        productName: 'IPhone 14 Pro',
-        price: 999.0,
+        productName: 'Heart Beads Earrings',
+        price: 59,
         productImage: product2,
-        description: 'The iPhone 14 Pro and iPhone 14 Pro Max are smartphones designed, developed, and marketed by Apple Inc. They are the sixteenth-generation flagship iPhones, succeeding the iPhone 13 Pro and iPhone 13 Pro Max. The devices were unveiled alongside the iPhone 14 and iPhone 14 Plus during the Apple Event at Apple Park in Cupertino, California, on September 7, 2022.[7][8] Pre-orders for the iPhone 14 Pro and 14 Pro Max began on September 9, 2022, and were made available on September 16, 2022.'
+        description: 'Spread love with these adorable heart beads earrings. Delicate and playful, they add a touch of sweetness to any look. Perfect for all occasions. Embrace romance and style with these heart beads earrings'
     },
     {
         id: 3,
-        productName: 'IPhone 14 Pro Max',
-        price: 999.0,
+        productName: 'Blue Eye Bracelet',
+        price: 49,
         productImage: product3,
-        description: 'The iPhone 14 Pro and iPhone 14 Pro Max are smartphones designed, developed, and marketed by Apple Inc. They are the sixteenth-generation flagship iPhones, succeeding the iPhone 13 Pro and iPhone 13 Pro Max. The devices were unveiled alongside the iPhone 14 and iPhone 14 Plus during the Apple Event at Apple Park in Cupertino, California, on September 7, 2022.[7][8] Pre-orders for the iPhone 14 Pro and 14 Pro Max began on September 9, 2022, and were made available on September 16, 2022.'
+        description: 'Embrace protection and style with this captivating blue eye bracelet. Adorned with a mesmerizing blue eye charm, it symbolizes warding off negative energy and bringing good luck. The vibrant blue color adds a pop of elegance, while the adjustable chain ensures a perfect fit. Wear this bracelet as a fashionable talisman and exude confidence wherever you go.'
     },
     {
         id: 4,
-        productName: 'Samsung Galaxy S23 ULTRA',
-        price: 999.0,
+        productName: 'Hologram Big O Bracelet',
+        price: 68,
         productImage: product4,
-        description: 'The Galaxy S23 Ultra has a squarer form factor than the rest of the series because it’s the only variant to adopt the S Pen. The back panel and display curve around the side edges, but only slightly. The S23 Ultra is flatter than the Galaxy Note series used to be.'
+        description: 'Step into the future of fashion with this eye-catching hologram Big O bracelet. Its mesmerizing design displays a spectrum of vibrant colors, creating a stunning visual display with every movement. The sleek and adjustable band ensures a comfortable fit for all wrist sizes. Make a bold statement and add a touch of futuristic style to your outfits with this one-of-a-kind hologram Big O bracelet.'
     },
     {
         id: 5,
-        productName: 'Nintendo Switch',
-        price: 999.0,
+        productName: 'Red Quartz Ring',
+        price: 89.99,
         productImage: product5,
-        description: 'hybrid video game console, consisting of a console unit, a dock, and two Joy-Con controllers. '
+        description: 'Ignite your style with this captivating red quartz ring. The fiery red hue of the quartz gemstone commands attention, exuding confidence and passion. Set in a sleek band, this ring is a perfect blend of boldness and elegance. Whether for a special occasion or everyday wear, this red quartz ring will add a touch of allure and sophistication to any ensemble. Elevate your jewelry collection with this stunning statement piece.'
     },
     {
         id: 6,
-        productName: 'PS5',
-        price: 999.0,
+        productName: 'Enchanted Curve Earrings',
+        price: 40,
         productImage: product6,
-        description: 'Experience lightning-fast loading with an ultra-high speed SSD, deeper immersion with support for haptic feedback1, adaptive triggers1 and 3D Audio*, and an all-new generation of incredible PlayStation games.'
+        description: 'Add a touch of modern elegance to your look with these stunning curve earrings. The gracefully curved design effortlessly enhances your style, making a subtle yet eye-catching statement. Crafted with precision and attention to detail, these earrings are perfect for both casual and formal occasions. Elevate your jewelry collection with these versatile and chic curve earrings that are sure to turn heads wherever you go.'
     },
     {
         id: 7,
-        productName: 'Ipad',
-        price: 999.0,
+        productName: 'Shimmering Cascade',
+        price: 199.99,
         productImage: product7,
-        description: ' runs iOS and ships with several popular Apple and third-party apps preinstalled, including Messages, Mail, FaceTime, Music, Photos and the Safari web browser. Users can also download additional free and paid apps through the Apple App Store.'
+        description: 'Make a bold and glamorous statement with this captivating black diamond ring. The striking black diamond centerpiece exudes mystery and sophistication, set in a sleek band that beautifully accentuates its allure. Perfect for those seeking a unique and dramatic piece, this black diamond ring is a testament to exquisite craftsmanship and timeless style. Embrace the allure of black and let this extraordinary ring elevate your jewelry collection to new heights.'
     },
     {
         id: 8,
-        productName: 'Macbook',
-        price: 999.0,
+        productName: 'Serendipity Bubble (1pcs)',
+        price: 55,
         productImage: product8,
-        description: ' a term used for a brand of Mac notebook computers that Apple started producing in 2006. The American multinational corporation created MacBook computers when it consolidated its PowerBook and iBook lines during its transition to Intel processor-based products. As of 2013, there are two types of MacBook computers: the base-level MacBook Air and the upper-level MacBook Pro.'
+        description: 'Delicately crafted, each earring features a cluster of tiny bubbles, adding a whimsical and lighthearted touch to your style. These earrings are perfect for both casual and dressy occasions, making them a versatile addition to your jewelry collection. Embrace a touch of fun and grace with these delightful bubble earrings that are sure to make you stand out.'
     },
-    
+    {
+        id: 9,
+        productName: 'Blue Radiant Stack',
+        price: 55,
+        productImage: product9,
+        description: 'Make a dazzling statement with this Blue Radiant Stack Necklace. The necklace features a captivating stack of radiant blue gemstones, each one emanating a brilliant sparkle. The stunning shades of blue create a harmonious and mesmerizing display, while the delicate chain adds a touch of elegance. This necklace is the perfect accessory to elevate any outfit, from casual to formal, and is sure to draw attention with its exquisite beauty. '
+    },
+    {
+        id: 10,
+        productName: 'Minimalist Celestial Sparkle',
+        price: 1.000,
+        productImage: product10,
+        description: 'Radiate celestial elegance with this Minimalist Celestial Sparkle Necklace. The necklace showcases a delicate celestial charm, adorned with shimmering crystals that reflect the light with a captivating sparkle. Its minimalist design exudes understated beauty, making it a versatile piece for any occasion. Whether worn alone or layered with other necklaces, this celestial-inspired necklace adds a touch of celestial magic and effortless style to your look. '
+    },
+    {
+        id: 11,
+        productName: 'Pearls Choker',
+        price: 29.99,
+        productImage: product11,
+        description: 'Indulge in timeless elegance with this exquisite Pearl Choker Necklace. The necklace features a stunning arrangement of lustrous pearls, delicately strung together to create a classic and sophisticated look. With its adjustable length, it gracefully sits at the base of the neck, enhancing your neckline with a touch of refinement. Whether for a formal event or a stylish everyday accessory, this pearl choker necklace adds an air of grace and sophistication to any outfit.'
+    },
+    {
+        id: 12,
+        productName: 'Serene Spirit',
+        price: 55,
+        productImage: product12,
+        description: 'Embrace mystique and elegance with this captivating Black Diamond Necklace. The sleek black diamond pendant on a delicate chain creates a bold and sophisticated look. Perfect for any occasion, this necklace exudes timeless allure and individuality. Make a statement with this stunning black diamond necklace.'
+    },
 ]
