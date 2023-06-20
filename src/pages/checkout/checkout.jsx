9f97e2ea90c9d6f6fa22b670e4e727aba2c2bf13
LeonardoRichie <91533882+LeonardoRichie@users.noreply.github.com>
2023-06-20 23:09:06 +0700

    Update checkout.jsx

--- src/pages/checkout/checkout.jsx
+++ src/pages/checkout/checkout.jsx
@@ -8,6 +8,9 @@ export const Checkout = () => {
         <Link to="/">
             <button>back</button>
           </Link>
+          <h1>payment will be to 001230830850285 BCA bank account</h1>
+          <h1>Tracking number will be send via email</h1>
+          <h1>Thank you</h1>
     </div>
   )
 }
